ba6e6a26c2b13085c1ce53baad62f5400a97f6c2
James Rowland <jrowlandm@gmail.com>
2025-04-09 11:24:29 +0100

    up to first round of experiments (#1)
    * up to first round of experiments
    
    * tidy up, run black, remove n_trials_block
    
    ---------
    
    Co-authored-by: unknown <neuromantic99>

--- hardware_definitions/hardware_definition.py
+++ hardware_definitions/hardware_definition.py
@@ -4,13 +4,17 @@
 
 from devices.audio_board import Audio_board
 from devices.breakout_1_2 import Breakout_1_2
+from devices.frame_trigger import Frame_trigger
 from devices.poke import Poke
 from devices.LED_strip import LEDStrip
+from pyControl.hardware import Rsync
 
 board = Breakout_1_2()
 
 # Instantiate Devices.
-orange_LED = LEDStrip(board.port_1)
-blue_LED = LEDStrip(board.port_2)
+orange_LED = LEDStrip(board.port_3, color="orange")
+blue_LED = LEDStrip(board.port_2, color="blue")
 
-speaker = Audio_board(board.port_3)
+speaker = Audio_board(board.port_4)
+frame_trigger = Frame_trigger(pin=board.BNC_1, pulse_rate=30)
+sync_output = Rsync(pin=board.BNC_2, mean_IPI=1000)  # Instantiate Rsync object on breakout board BNC_1
